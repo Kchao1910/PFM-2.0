a6da5da422e90c6c9c425bd96e54eeabdaf945a2
Kchao1910 <kchao1910@gmail.com>
2019-04-21 02:08:09 -0700

    Removing nodes from budget page

---
+++ removeNodes.js
@@ -0,0 +1,30 @@
+function removeNodesFromMainSite() {
+  const body = document.getElementById("body");
+  
+  for (let i = 0; i < body.childElementCount - 1; i++) {
+    body.removeChild(body.lastChild);
+  }
+
+  return body;
+}
+
+function removeNodesFromCategoryContainers() {
+  const categoryContainers = document.querySelectorAll(".category-containers");
+
+  if (categoryContainers[0].hasChildNodes() === false) {
+    alert("No more categories to remove!");
+    return;
+  }
+
+  // remove category from budget
+  for (let i = 7; i < categoryContainers.length; i--) {
+    if (categoryContainers[i].hasChildNodes() === true) {
+      for (let j = 0; j < 6; j++) {
+        categoryContainers[i].removeChild(categoryContainers[i].firstChild);
+      }
+      break;
+    } else {
+      continue;
+    }
+  }
+}
